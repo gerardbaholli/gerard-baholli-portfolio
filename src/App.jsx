bc8b57edeebe66f84cb47e424fd8c9a78958988a
Gerard Baholli <gerardbaholli@live.it>
2024-12-25 17:57:24 +0100

    Added HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import MTGKeywordFinder from "./components/MTGKeywordFinder/MTGKeywordFinder.jsx";
 import Projects from "./components/Projects/Projects.jsx";
@@ -6,17 +6,13 @@ import NotFound from "./components/NotFound/NotFound.jsx";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/MTGKeywordFinder" element={<MTGKeywordFinder />} />
-          {/* Aggiungi altre rotte per altri progetti */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/MTGKeywordFinder" element={<MTGKeywordFinder />} />
+      {/* Aggiungi altre rotte per altri progetti */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { HashRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <Router basename="/gerard-baholli-portfolio"> */}
+    <Router>
+      <App />
+    </Router>
   </StrictMode>,
 )
